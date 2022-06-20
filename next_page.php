6af8419332fc0c48f4690b851a5c481349af4f64
nmieza <nurmieza14@gmail.com>
2022-06-20 13:15:57 +0800

    cookies

--- next_page.php
+++ next_page.php
@@ -7,33 +7,30 @@
     <title>Document</title>
 </head>
 <body>
+    
     <?php
-
     session_start();
     echo "<html>";
 
-    /*$sid = session_id();
-    echo "Session ID returned by session_id(): ".$sid."\n";
-    $sid = SID;
-    echo "Session ID returned by SID: ".$sid."\n";
-    echo "<br>";*/
-
     $queryString = $_SERVER["QUERY_STRING"];
-    echo "Query string of the incoming URL: ".$queryString."\n";
-    
-    echo "Cookies received: \n";
-    foreach ($_COOKIE as $name => $value) {
-        echo " $name = $value\n";
+    echo "Query string of the incoming URL: ".$queryString."<br>";
+
+    echo "Cookies received:<br>";
+    foreach ($_COOKIE as $name => $value){
+        echo " $name = $value<br>";
     }
 
-    $myLogin = $_SESSION["myLogin"];
-    echo "Value of myLogin has been retrieved: ".$myLogin."\n";
+    $sid = session_id();
+    echo "Session ID returned by session_id(): ".$sid."<br>";
+    $sid = SID;
+    echo "Session ID returned by SID: ".$sid."<br>";
 
+    $myLogin = $_SESSION["myLogin"];
+    echo "Value of myLogin has been retrieved: ".$myLogin."<br>";
     $myColor = $_SESSION["myColor"];
-    echo "Value of MyColor has been retrieved: ".$myColor."\n";
+    echo "Value of myColor has been retrieved: ".$myColor."<br>";
 
     echo "</html>\n";
-
     ?>
 
 </body>
