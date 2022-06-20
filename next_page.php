5c5f84dbfe26ac06c88d39ab12e9aa8b34843af0
nmieza <nurmieza14@gmail.com>
2022-06-20 13:04:03 +0800

    commit

--- next_page.php
+++ next_page.php
@@ -12,10 +12,11 @@
     session_start();
     echo "<html>";
 
-    $sid = session_id();
+    /*$sid = session_id();
     echo "Session ID returned by session_id(): ".$sid."\n";
     $sid = SID;
     echo "Session ID returned by SID: ".$sid."\n";
+    echo "<br>";*/
 
     $myLogin = $_SESSION["myLogin"];
     echo "Value of myLogin has been retrieved: ".$myLogin."\n";
